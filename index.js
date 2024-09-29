fb54e0d8cf8dbe0b531ce2e4f2d3e5c65df190d0
Jondojd <165144273+Jondojd@users.noreply.github.com>
2024-09-29 13:30:50 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const heading = document.querySelector('h1');
+    heading.textContent = "Bienvenue sur mon site déployé sur GitHub Pages!";
+});
